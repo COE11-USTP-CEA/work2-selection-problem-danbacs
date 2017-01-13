0672f1528e4f29fbd1d37fcc81d14a91fc17f975
Dan Timothy Baconga <danbaconga180@gmail.com>
2017-01-13 19:09:42 +0800

    Update item.java

--- item.java
+++ item.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package inventory;
-
-/**
- *
- * @author Core i3
- */
 public class item {
     
     String item_name;
